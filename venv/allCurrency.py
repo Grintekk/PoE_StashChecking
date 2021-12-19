5d8fe848875a0ca9fa52957f4ca515305f309407
Grintekk <grintekk@gmail.com>
2021-12-19 16:49:28 +0200

    download icons

--- venv/allCurrency.py
+++ venv/allCurrency.py
@@ -1,13 +1,20 @@
 import json
+import requests
+import os
 
 with open('items.json') as outfile:
     dataItems = json.load(outfile)
 
 all_currency = []
 #dataItems = dataItems['stash']
-count = 0
+location = os.path.abspath("C:/Users/Admin/PycharmProjects/PoE_StashChecking/venv/Icons/")
 for i in dataItems['items']:
+    img_data = requests.get(i['icon']).content
+
+    with open( location + '\\' +str(i['typeLine'])+'.png', 'wb') as handler:
+        handler.write(img_data)
     #all_currency[count] = [i['stackSize'],i['typeLine']]
-    if i.get('stackSize'):
-        all_currency.append([i['typeLine'],i['stackSize']])
+    #if i.get('stackSize'):
+    #    all_currency.append([i['typeLine'],i['stackSize']])
+
 print(all_currency)
